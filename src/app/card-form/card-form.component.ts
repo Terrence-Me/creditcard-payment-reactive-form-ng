4773954362b1c557b9d7bdfbffff617537bfe9e9
Terrence Meikle <terrence_meikle@yahoo.com>
2023-02-28 19:46:46 -0500

    add initial validation

--- src/app/card-form/card-form.component.ts
+++ src/app/card-form/card-form.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-card-form',
@@ -7,7 +7,7 @@ import { FormGroup, FormControl } from '@angular/forms';
   styleUrls: ['./card-form.component.css'],
 })
 export class CardFormComponent {
-  constForm = new FormGroup({
-    name: new FormControl(''),
+  cardForm = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.minLength(3)]),
   });
 }
